c9b58742b85eef39bba47461478b1b3e9fc26ef6
Andrew Ruzavin <andrew.ruzavin@gmail.com>
2023-06-15 17:50:13 +0700

    Initial Commit

---
+++ m16.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16.5
+//
+//  Created by Андрей Рузавин on 15.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
